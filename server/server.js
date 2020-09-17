52d5e2763c6899cb33207665a234bcdff3329e72
Raul Condori <raul.condori@gmail.com>
2020-09-17 18:00:40 -0300

    Restserver - usuario

--- server/server.js
+++ server/server.js
@@ -15,42 +15,12 @@ app.use(bodyParser.json())
 
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-});
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+app.use( require('./routes/usuario'));
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else
-
-        res.json({
-        persona: body
-    });
-
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario');
-});
-
-
-mongoose.connect('mongodb://localhost:27017/cafe', (err,res) => {
+mongoose.connect(process.env.URLDB , 
+                {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true},    
+                (err,res) => {
 
     if (err) throw err;
 
